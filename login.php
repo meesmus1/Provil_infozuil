74adbab2312acba5e7fcc4cb3aaf4d829539669b
mees <mees.vanderheijden@provil.be>
2024-11-26 08:55:42 +0100

    login styling en backend

--- login.php
+++ login.php
@@ -13,16 +13,27 @@ if ($_SESSION && $_SESSION['user']) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Provil | login</title>
+
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+  <script src="./js/index.js"></script>
+
 </head>
 
 <body>
 
-  <form id="login" action="" method="post">
-    <input type="text" name="username" value="" placeholder="username">
-    <input type="password" name="password" value="" placeholder="password">
-    <input id="loginButton" type="submit" name="" value="login">
-  </form>
-  <div class="alert"></div>
+  <header>
+    <form id="login" action="" method="post">
+      <input type="text" name="username" value="" placeholder="username">
+      <input type="password" name="password" value="" placeholder="password">
+      <input id="loginButton" type="submit" name="" value="login">
+    </form>
+    <div class="alert"></div>
+  </header>
 
 </body>
 
--- signup.php
+++ signup.php
@@ -4,30 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sing up</title>
+    <title>Signup</title>
+
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <!-- style sheet-->
+    <link rel="stylesheet" href="css\style.css">
 </head>
 
 <body>
 
-    <div class="container mt-6">
-        <div class="row">
-            <div class="col-6 mb-4">
-                <label class="form-label">Username</label>
-                <input type="text" id="username" class="form-control" placeholder="username">
-            </div>
-            <div class="col-6 mb-4">
-                <label class="form-label">Password</label>
-                <input type="text" id="Password" class="form-control" placeholder="Password">
-            </div>
-            <div class="col-12">
-                <button type="button" class="btn btn-primary" onclick="register()">Registreer</button>
-                <p class="alert alert-danger mt-3" id="errorMessage" style="display:none">Niet alle velden zijn ingevuld.</p>
+    <section class="vh-100" style="background-color: #508bfc;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100 mt-3">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+
+                            <h3 class="mb-5">Signup dabaj!</h3>
+
+                            <div id="fout" class=""></div>
+
+                            <div class="form-outline mb-4">
+                                <input type="username" id="username" class="form-control form-control-lg" placeholder="username:" />
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <input type="password" id="password" class="form-control form-control-lg" placeholder="Password:"/>
+                            </div>
+
+                            <button class="btn btn-primary btn-lg btn-block" type="submit" onclick="Signup()">Signup</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>    
-
+    </section>
 </body>
 
-</html>
-
+<!-- dikke javascript -->
 <script src="./js/signup.js"></script>
+
+</html>
